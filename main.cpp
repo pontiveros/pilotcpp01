e2175876cd2f2f734d4989c444ce3145dbfda691
Pedro Ontiveros <ontiveros.pedro@gmail.com>
2024-05-30 16:07:40 -0400

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* ********************************** */
+/* ********************************** */
+/* **** File: main.cpp           **** */
+/* **** Compile: g++ main.cpp    **** */
+/* **** Debug: gdb               **** */
+/* ********************************** */
+/* ********************************** */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) 
+{
+    std::cout << "Hello from pilotcpp01 app, this is only for testing purposes." << std::endl;
+    return 0;
+}
